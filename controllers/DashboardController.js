7f20de02d290f60b9eec7530309a744e6d832d0e
Jay Momnang <jaymomnang@users.noreply.github.com>
2018-01-08 13:29:25 +0100

    changes test

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,7 +1,17 @@
 'use strict';
 exports.loadDashboard = function (req, res) {
   //TODO: Get dashboard data
-  res.render("dashboard");
+  var _url = mc_api + "schedule/";
+  request(_url, function (error, response, body) {
+
+    if (error) return error;
+    var data = JSON.parse(body);
+
+    var ui_data = req.session;
+    res.render("dashboard", { menus, ui_data, data });
+
+  });
+
 };
 
 exports.getSchedules = function(req, res){
