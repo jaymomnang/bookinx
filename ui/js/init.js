cba0528b6bf148eacd950bab3a40171dc0ef5088
jaymomnang <juliusmomnang@gmail.com>
2018-02-07 07:25:53 +0100

    ui changes

--- ui/js/init.js
+++ ui/js/init.js
@@ -1,7 +1,16 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
+        $('select').material_select();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15, // Creates a dropdown of 15 years to control year,
+            today: 'Today',
+            clear: 'Clear',
+            close: 'Ok',
+            closeOnSelect: false // Close upon selecting a date,
+        });
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
