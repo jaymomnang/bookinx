de09f17fe1479d9305cec3e2cb0e5f22a94f2b81
jaymomnang <juliusmomnang@gmail.com>
2018-03-15 03:22:40 +0100

    bug fixes schedules

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -22,7 +22,7 @@ exports.list_all_schedules = function(req, res) {
                 if (err) return err;
 
                 var _ports = JSON.parse(p);
-                var _p = getRoutes(_ports);
+                var _routes = getRoutes(_ports);
 
 
                 _url = mc_api + "price"
@@ -31,7 +31,7 @@ exports.list_all_schedules = function(req, res) {
                     var _prices = JSON.parse(p);
                     var ui_data = req.session;
 
-                    res.render("schedules", { menus, ui_data, schedules, _vs, _prices, _p });
+                    res.render("schedules", { menus, ui_data, schedules, _vs, _prices, _routes });
 
                 })
 
@@ -53,35 +53,42 @@ exports.add_schedule = function(req, res) {
     req.body.departure_port = _data1[0];
     req.body.destination = _data1[1];
 
-    console.log(req.body);
-
     var url_partial = "schedule";
     var auth_url = mc_api + url_partial;
     request.post({ headers: { 'content-type': 'application/x-www-form-urlencoded' }, url: auth_url, form: req.body }, function(error, response, body) {
-        //var schedules = JSON.parse(body);
-        console.log(body);
+        var schedules = JSON.parse(body);
 
         var msg = 'Error creating schedule, Please contact your administrator';
         var failed = true;
         if (!error) {
             failed = false;
-            var _cl;
             var _vs;
-
-            var _url2 = mc_api + "class";
-            request(_url2, function(err, resp, b) {
+            var _url = mc_api + "Vessel";
+            request(_url, function(err, resp, b) {
                 if (err) return err;
-                _cl = JSON.parse(b);
+                _vs = JSON.parse(b);
 
-                var _url3 = mc_api + "Vessel";
-                request(_url3, function(err, resp, b) {
+                _url = mc_api + "ports"
+                request(_url, function(err, resp, p) {
                     if (err) return err;
-                    _vs = JSON.parse(b);
 
-                    var ui_data = req.session;
-                    res.render("schedules", { menus, ui_data, _vs, _cl });
+                    var _ports = JSON.parse(p);
+                    var _routes = getRoutes(_ports);
+
+
+                    _url = mc_api + "price"
+                    request(_url, function(err, resp, p) {
+                        if (err) return err;
+                        var _prices = JSON.parse(p);
+                        var ui_data = req.session;
+
+                        res.render("schedules", { menus, ui_data, schedules, _vs, _prices, _routes });
+
+                    })
+
                 })
             })
+
         }
     });
 
